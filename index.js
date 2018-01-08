54e683e8c0fd6223a327961a716e72af213d8981
Bruno <zettca@gmail.com>
2018-01-08 15:32:02 +0000

    Remove unused DB/API code Add compression

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const url = require('url');
 const express = require('express');
+const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const lib = require('./lib/helpers.js');
 
 const app = express();
 
+app.use(compression());
 app.use(cookieParser());
-app.use('/app', lib.requireAuth, express.static(__dirname + "/client/build/"));
+app.use('/app', express.static(__dirname + "/client/build/"));
 
 app.get('/', lib.handleRequests);
 app.get('/auth', lib.handleAuth);
 
-app.get('/item/:id', lib.handleReqItem);
-app.get('/stories/:cat', lib.handleReqStories);
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server running at ${port}...`);
+    console.log(`App running at ${port}...`);
 });
