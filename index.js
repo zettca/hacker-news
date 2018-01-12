9f71c795eefebe368aa6bbdc7264b90796d1cde5
Bruno <zettca@gmail.com>
2018-01-12 21:49:26 +0000

    Improve API Authentication Replace Cookie uuid auth with JWT. Add server 'cached' API with pagination

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const url = require('url');
+const cors = require('cors')
+const jwt = require('express-jwt');
 const express = require('express');
 const compression = require('compression');
-const cookieParser = require('cookie-parser');
+
 const lib = require('./lib/helpers.js');
+const config = require('./config.json');
 
 const app = express();
 
 app.use(compression());
-app.use(cookieParser());
-app.use('/app', express.static(__dirname + "/client/build/"));
+app.use(cors({ credentials: true, origin: true }));
 
-app.get('/', lib.handleRequests);
-app.get('/auth', lib.handleAuth);
+// serve React app
+const clientPath = __dirname + "/client/build";
+app.use('/', express.static(clientPath));
+app.use("/static", express.static(clientPath + "/static"));
+
+app.get('/auth/github/:code', lib.handleAuth);
+
+app.all('/api/*', jwt({ secret: config.secret }), lib.validateUser);
+app.get('/api/item/:id', lib.handleAPIItems);
+app.get('/api/stories/:cat/:page', lib.handleAPIStories);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
